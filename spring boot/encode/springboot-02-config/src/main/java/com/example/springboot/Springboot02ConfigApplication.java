00cb0d47f81328edd5aa6aa86dfffa64dffa28e8
baimuyunxi <1365402987@qq.com>
2023-02-11 15:27:51 +0800

    随堂笔记+源码

--- spring boot/encode/springboot-02-config/src/main/java/com/example/springboot/Springboot02ConfigApplication.java
+++ spring boot/encode/springboot-02-config/src/main/java/com/example/springboot/Springboot02ConfigApplication.java
@@ -2,8 +2,10 @@ package com.example.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @SpringBootApplication
+@ConfigurationProperties(prefix = "spring.http")
 public class Springboot02ConfigApplication {
 
     public static void main(String[] args) {
