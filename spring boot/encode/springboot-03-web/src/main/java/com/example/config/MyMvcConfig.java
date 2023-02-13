da899522d6f88f8aff3143813fa55259d1a67058
baimuyunxi <62205616+baimuyunxi@users.noreply.github.com>
2023-02-13 23:27:42 +0800

    feat：springMVC扩展配置

--- spring boot/encode/springboot-03-web/src/main/java/com/example/config/MyMvcConfig.java
+++ spring boot/encode/springboot-03-web/src/main/java/com/example/config/MyMvcConfig.java
@@ -1,30 +1,18 @@
 package com.example.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.View;
-import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Locale;
 
 // 如果想要diy一些定制化的功能 只要写这个组件 然后将它交给springboot springboot就会帮忙自动装配
 // 扩展 springmvc dispatchservlet
 @Configuration
 public class MyMvcConfig implements WebMvcConfigurer {
 
-    // ViewResolver 实现了视图解析器接口的类 就可以把它看做视图解析器
-    @Bean
-    public ViewResolver myViewResolver(){
-        return new MyViewResolver();
+    // 视图跳转
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+//        WebMvcConfigurer.super.addViewControllers(registry);
+        registry.addViewController("/yunxi").setViewName("index1");
     }
-
-    // 定义了一个视图解析器MyViewResolver
-    public static class MyViewResolver implements ViewResolver{
-        @Override
-        public View resolveViewName(String s, Locale locale) throws Exception {
-            return null;
-        }
-    }
-
 }
