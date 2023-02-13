8e7fe818149e688432f1aef1cd58ced0908d5d90
baimuyunxi <62205616+baimuyunxi@users.noreply.github.com>
2023-02-13 22:28:31 +0800

    feat：tyemaleaf简单配置说明

--- spring boot/encode/springboot-03-web/src/main/java/com/example/controlle/IndexController.java
+++ spring boot/encode/springboot-03-web/src/main/java/com/example/controlle/IndexController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Arrays;
+
 //在template目录下的所有页面，只能通过controller来跳转
 //这个需要模板引擎的支持 thymeleaf
 @Controller
@@ -11,10 +13,12 @@ public class IndexController {
 
     @RequestMapping("/index1")
     public String index1(Model model){
-        model.addAttribute("mass","hello!Yunxi");
+        model.addAttribute("mass","<h1>hello!Yunxi</h1>");
         model.addAttribute("mass_sty","line-height: 80px;" + "text-align: center;\n" + "background-color: #4158D0;\n" +
                 "background-image: linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%);\n"
                 + "height: 80px;\n" + "width: 200px;\n");
+
+        model.addAttribute("user", Arrays.asList("Yunxi","Mumu"));
         return "index1";
     }
 }
