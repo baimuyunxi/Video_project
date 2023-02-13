84ddd37ea821063afef1108982cf24fd3bb28d42
baimuyunxi <62205616+baimuyunxi@users.noreply.github.com>
2023-02-13 22:06:21 +0800

    feat：thymeleaf初体验

--- spring boot/encode/springboot-03-web/src/main/java/com/example/controlle/IndexController.java
+++ spring boot/encode/springboot-03-web/src/main/java/com/example/controlle/IndexController.java
@@ -1,6 +1,7 @@
 package com.example.controlle;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 //在template目录下的所有页面，只能通过controller来跳转
@@ -9,7 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 
     @RequestMapping("/index1")
-    public String index(){
+    public String index1(Model model){
+        model.addAttribute("mass","hello!Yunxi");
+        model.addAttribute("mass_sty","line-height: 80px;" + "text-align: center;\n" + "background-color: #4158D0;\n" +
+                "background-image: linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%);\n"
+                + "height: 80px;\n" + "width: 200px;\n");
         return "index1";
     }
 }
