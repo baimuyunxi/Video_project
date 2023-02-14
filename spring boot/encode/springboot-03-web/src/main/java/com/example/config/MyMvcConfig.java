2583512225bf8601c4174ff27455a5bb87020b6b
baimuyunxi <62205616+baimuyunxi@users.noreply.github.com>
2023-02-14 22:57:43 +0800

    feat：员工管理系统准备工作
    通过java语言伪造数据库的增删改查，同时导入一些页面文件

--- spring boot/encode/springboot-03-web/src/main/java/com/example/config/MyMvcConfig.java
+++ spring boot/encode/springboot-03-web/src/main/java/com/example/config/MyMvcConfig.java
@@ -1,18 +1,9 @@
 package com.example.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// 如果想要diy一些定制化的功能 只要写这个组件 然后将它交给springboot springboot就会帮忙自动装配
-// 扩展 springmvc dispatchservlet
 @Configuration
 public class MyMvcConfig implements WebMvcConfigurer {
 
-    // 视图跳转
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-//        WebMvcConfigurer.super.addViewControllers(registry);
-        registry.addViewController("/yunxi").setViewName("index1");
-    }
 }
