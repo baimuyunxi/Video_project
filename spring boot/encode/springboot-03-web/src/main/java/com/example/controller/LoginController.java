750bf5dad0c94baa24ef8c69ea708bc00bad5ac2
baimuyunxi <62205616+baimuyunxi@users.noreply.github.com>
2023-02-15 23:18:22 +0800

    feat：登录功能的实现
    dubug：在密码验证成功时候，转跳到后台页面会500

---
+++ spring boot/encode/springboot-03-web/src/main/java/com/example/controller/LoginController.java
@@ -0,0 +1,26 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.util.StringUtils;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/user/login")
+    public String login(@RequestParam("username") String username , @RequestParam("password") String password ,
+                        Model model){
+
+        // 具体业务
+        if (!StringUtils.isEmpty(username) && "123456".equals(password))
+            return "redirect:/main.html";
+        else{
+            // 登录失败
+            model.addAttribute("msg","用户名或者密码错误");
+            return "index";
+        }
+    }
+}
