ef1e3e6d5b43a5123e95ca91950354a05c17672c
baimuyunxi <62205616+baimuyunxi@users.noreply.github.com>
2023-02-15 23:44:53 +0800

    feat：登录拦截器

--- spring boot/encode/springboot-03-web/src/main/java/com/example/config/MyMvcConfig.java
+++ spring boot/encode/springboot-03-web/src/main/java/com/example/config/MyMvcConfig.java
@@ -3,6 +3,7 @@ package com.example.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,5 +20,11 @@ public class MyMvcConfig implements WebMvcConfigurer {
     @Bean
     public LocaleResolver localeResolver(){
         return new MyLocaleResolver();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**").excludePathPatterns("/index" +
+                ".html","/","/user/login","/css/*","/img/*","/js/*");
     }
 }
--- spring boot/encode/springboot-03-web/src/main/java/com/example/controller/LoginController.java
+++ spring boot/encode/springboot-03-web/src/main/java/com/example/controller/LoginController.java
@@ -7,17 +7,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.thymeleaf.util.StringUtils;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class LoginController {
 
     @RequestMapping("/user/login")
     public String login(@RequestParam("username") String username , @RequestParam("password") String password ,
-                        Model model){
+                        Model model, HttpSession session){
 
         // 具体业务
-        if (!StringUtils.isEmpty(username) && "123456".equals(password))
+        if (!StringUtils.isEmpty(username) && "123456".equals(password)){
+            session.setAttribute("loginUser",username);
             return "redirect:/main.html";
-        else{
+        } else{
             // 登录失败
             model.addAttribute("msg","用户名或者密码错误");
             return "index";
