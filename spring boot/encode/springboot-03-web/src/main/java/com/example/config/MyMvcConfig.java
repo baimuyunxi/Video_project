1819fd377b5c6ce26f4704a1f1c4581ce70b1b14
baimuyunxi <62205616+baimuyunxi@users.noreply.github.com>
2023-02-15 22:46:02 +0800

    feat：页面国际化

--- spring boot/encode/springboot-03-web/src/main/java/com/example/config/MyMvcConfig.java
+++ spring boot/encode/springboot-03-web/src/main/java/com/example/config/MyMvcConfig.java
@@ -1,6 +1,8 @@
 package com.example.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +12,11 @@ public class MyMvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
         registry.addViewController("/index.html").setViewName("index");
+    }
+
+    // 自定义国际化的组件生效
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new MyLocaleResolver();
     }
 }
