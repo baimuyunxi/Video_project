0e226a2f5c272d7f274aeef04ec0cd9a79ca6022
baimuyunxi <62205616+baimuyunxi@users.noreply.github.com>
2023-02-17 21:10:23 +0800

    feat：页面数据库的一个增删

--- spring boot/encode/springboot-03-web/src/main/java/com/example/controller/EmployeeController.java
+++ spring boot/encode/springboot-03-web/src/main/java/com/example/controller/EmployeeController.java
@@ -1,10 +1,14 @@
 package com.example.controller;
 
+import com.example.dao.DepartmentDao;
 import com.example.dao.EmployeeDao;
+import com.example.pojo.Department;
 import com.example.pojo.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Collection;
@@ -14,11 +18,28 @@ public class EmployeeController {
 
     @Autowired
     EmployeeDao employeeDao;
+    @Autowired
+    DepartmentDao departmentDao;
 
     @RequestMapping("/emps")
-    public String list(Model model){
+    public String list(Model model) {
         Collection<Employee> employees = employeeDao.getAll();
-        model.addAttribute("emps",employees);
+        model.addAttribute("emps", employees);
         return "emp/list";
+    }
+
+    @GetMapping("emp")
+    public String toAddpage(Model model) {
+        // 查出所有部门信息
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("departments", departments);
+        return "emp/add";
+    }
+
+    @PostMapping("/emp")
+    public String addEmp(Employee employee) {
+        System.out.println("save==>" + employee);
+        employeeDao.save(employee);
+        return "redirect:/emps";
     }
 }
