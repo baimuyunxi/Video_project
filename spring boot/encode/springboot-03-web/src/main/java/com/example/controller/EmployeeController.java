6233be368a9a2c16794976304bda7e01fe4cf24e
baimuyunxi <62205616+baimuyunxi@users.noreply.github.com>
2023-02-17 21:39:10 +0800

    feat：修改员工信息

--- spring boot/encode/springboot-03-web/src/main/java/com/example/controller/EmployeeController.java
+++ spring boot/encode/springboot-03-web/src/main/java/com/example/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -42,4 +43,25 @@ public class EmployeeController {
         employeeDao.save(employee);
         return "redirect:/emps";
     }
+
+    @GetMapping("/emp/{id}")
+    // 去员工的修改页面
+    public String toUpdateEmp(@PathVariable("id") Integer id, Model model) {
+        // 查出原来的数据
+        Employee employee = employeeDao.getEmployeeById(id);
+        model.addAttribute("emp", employee);
+        // 查出所有部门信息
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("departments", departments);
+        return "emp/update";
+    }
+
+    @RequestMapping("/updateEmp")
+    public String updateEmp(Employee employee){
+        employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+
+   
+
 }
