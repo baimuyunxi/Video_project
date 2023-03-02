6d12f90e67a33eb4d5517f93ec8c44ef9c6dcefc
baimuyunxi <62205616+baimuyunxi@users.noreply.github.com>
2023-03-02 22:09:07 +0800

    fix：解决swagger高版本启动报错

--- spring boot/encode/springboot_swagger/src/main/java/com/example/config/SwaggerConfig.java
+++ spring boot/encode/springboot_swagger/src/main/java/com/example/config/SwaggerConfig.java
@@ -3,8 +3,10 @@ package com.example.config;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+
 @Configuration
 @EnableSwagger2
+//@EnableWebMvc
 public class SwaggerConfig {
 
 }
